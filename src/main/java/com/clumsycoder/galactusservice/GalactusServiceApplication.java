07205baaa4c1b5654432647634bbf0b095dcefe4
Gaurav Aggarwal <gauravxor@gmail.com>
2025-06-03 02:01:15 +0530

    Initial commit

---
+++ src/main/java/com/clumsycoder/galactusservice/GalactusServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clumsycoder.galactusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalactusServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalactusServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clumsycoder/galactusservice/GalactusServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clumsycoder.galactusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalactusServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
